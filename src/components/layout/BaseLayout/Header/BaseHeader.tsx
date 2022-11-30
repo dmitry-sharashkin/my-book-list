a7a1c76c7cf58447b766ec77e09f61d15e68c7ff
dmitry-sharashkin <faustfbs@mail.ru>
2022-11-30 17:23:25 +0300

    add header

---
+++ src/components/layout/BaseLayout/Header/BaseHeader.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { NavLink } from "react-router-dom";
+import logo from "../../../../assets/icons/book-logo.svg";
+
+import styles from "./styles.module.scss";
+
+export default function BaseHeader() {
+  return (
+    <>
+      <header className={styles.container}>
+        <NavLink to={"/"} className={styles.logo}>
+          <img src={logo} alt="logo" className={styles.logoIcon} />
+          <span className={styles.logoTitle}>My book list </span>
+        </NavLink>
+        <ul>
+          <li>
+            <NavLink to={"/"}>Home </NavLink>
+          </li>
+          <li>
+            {" "}
+            <NavLink to={"/auth"}>Auth </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/feed"}>Feed </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/404"}>404 </NavLink>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
+}
